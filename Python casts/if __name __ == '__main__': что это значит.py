5fbea7584cfccc08d076de735ef93ad1634ed880
Nikolai Timofeev <extezy34@gmail.com>
2019-03-09 13:16:01 +0300

    Про __name__ == '__main__'

---
+++ Python casts/if __name __ == '__main__': что это значит.py
@@ -0,0 +1,16 @@
+a = '123'
+
+def add():
+    pass
+
+
+# print(globals())
+
+# print('Imported from:', __name__)
+
+def main():  # данная функция обозначает точку входа
+    pass
+
+if __name__ == '__main__':
+        main()
+
